4288a40c36c55b0be711fbccb666a07a920c6b79
Fedor Zhekov <zhekovfi@gmail.com>
2023-04-01 22:18:45 +0300

    added github actions pipeline

--- build/Build.cs
+++ build/Build.cs
@@ -1,6 +1,22 @@
 using Bimlab.Nuke;
 using Bimlab.Nuke.Components;
+using Nuke.Common.CI.GitHubActions;
 
+[GitHubActions("CI",
+    GitHubActionsImage.WindowsLatest,
+    FetchDepth = 0,
+    OnPushBranches = new[]
+    {
+        "develop"
+    },
+    InvokedTargets = new[]
+    {
+        nameof(IPublish.Publish)
+    },
+    ImportSecrets = new[]
+    {
+        "NUGET_API_KEY", "ALL_PACKAGES"
+    })]
 class Build : BimLabBuild, IPublish, ITest
 {
     public static int Main() => Execute<Build>(x => x.From<ICompile>().Compile);
