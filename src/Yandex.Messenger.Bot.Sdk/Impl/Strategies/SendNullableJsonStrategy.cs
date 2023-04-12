e5074ba5bc0c46e6145eafa74a9ac75f4925b54a
Max Eroshkin <dealstone@gmail.com>
2023-04-12 20:18:36 +0300

    Fixed webhook reset (setting webhook url to null)

---
+++ src/Yandex.Messenger.Bot.Sdk/Impl/Strategies/SendNullableJsonStrategy.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Text.Json;
+using Yandex.Messenger.Bot.Sdk.Json;
+
+namespace Yandex.Messenger.Bot.Sdk.Impl.Strategies;
+
+/// <inheritdoc />
+internal class SendNullableJsonStrategy : ISendStrategy
+{
+    private readonly string _endpoint;
+    private readonly HttpMethod _method;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SendJsonStrategy"/> class.
+    /// </summary>
+    /// <param name="endpoint">An endpoint relative url.</param>
+    /// <param name="method">The http method.</param>
+    public SendNullableJsonStrategy(string endpoint, HttpMethod? method = null!)
+    {
+        _endpoint = endpoint;
+        _method = method ?? HttpMethod.Post;
+    }
+
+    /// <inheritdoc/>
+    public HttpRequestMessage CreateRequest(object payload)
+    {
+        return new HttpRequestMessage(_method, _endpoint)
+        {
+            Content = new StringContent(
+                JsonSerializer.Serialize(payload, YandexMessengerBotNullableJsonOptions.Value),
+                Encoding.UTF8,
+                "application/json")
+        };
+    }
+}
--- src/Yandex.Messenger.Bot.Sdk/Models/Responses/SetWebhookResponse.cs
+++ src/Yandex.Messenger.Bot.Sdk/Models/Responses/SetWebhookResponse.cs
@@ -1,4 +1,12 @@
 ﻿namespace Yandex.Messenger.Bot.Sdk.Models.Responses;
 
 /// <inheritdoc />
-public record SetWebhookResponse(bool Ok, string Description) : Response(Ok, Description);
+public record SetWebhookResponse(
+        bool Ok,
+        string Description,
+        string Id,
+        string DisplayName,
+        object WebhookUrl,
+        long[] Organizations,
+        string Login)
+    : Response(Ok, Description);
